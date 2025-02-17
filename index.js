afdfeab7aa353f5b451839a83fdf6e7a992b9ff1
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2025-02-17 21:34:55 +0300

    ...

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ List something /help."
 index.help(ctx => ctx.reply([1, 2, 3]))
 index.on('message', async (ctx) => {
     const userText = ctx.message.text
+    if (ctx.message.text == 'пшик') {
+        ctx.reply('i love dariya')
+        return
+    }
     ctx.reply('123' + userText)
 })
 index.launch()
