b1c14c49300eb2e3ce513a5160eb0a7f0f6c705a
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2025-02-17 19:16:39 +0300

    ...

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { Telegraf } = require('telegraf');
 const index = new Telegraf('7696371667:AAH2qjQQOssN3SXCc3kId1UHifWArCd5Ikw')
 
 index.start(ctx => ctx.reply(`
-    Hi ${ctx.from.first_name}! 
-    List something /help."
+Hi ${ctx.from.first_name}! 
+List something /help."
 `))
 index.help(ctx => ctx.reply([1, 2, 3]))
 index.on('message', async (ctx) => {
